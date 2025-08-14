a467dfff12617e03b198ee3d68f299b62f22b9d9
Ethan Triem <EJTriem@outlook.com>
2025-08-13 21:54:37 -0500

    time out too many request may not be do able tbh

--- Connect.py
+++ Connect.py
@@ -1,16 +1,35 @@
 from garminconnect import Garmin
 import datetime
-from datetime import date
 import pandas as pd
 import matplotlib.pyplot as plt
 import getpass
 
+TEST_DATE = datetime.date(2025, 8, 12)
+
 if __name__ == "__main__":
 
-    username = input("Enter email address: ")
-    password = getpass("Enter password: ")
+    #username = input("Enter email address: ")
+    #password = getpass("Enter password: ")
 
-    api = Garmin(username, password).login()
+    username = "EJTriem@outlook.com"
+    password = "Ou812@atm8023"
+
+    api = Garmin(username, password)
+    api.login()
 
     activity_start_date = datetime.date(2025, 7, 1)
-    activity_end_date = date.today()
+    activity_end_date = datetime.date.today()
+
+    activities = api.get_activities_by_date(
+                activity_start_date.isoformat(),
+                activity_end_date.isoformat()
+    )
+
+    activity_df = pd.DataFrame(activities)
+
+    sleep = api.get_sleep_data(TEST_DATE) 
+
+    #while activity_start_date <= activity_end_date:
+
+
+    print(sleep["summary"]["overallScore"]["value"])
