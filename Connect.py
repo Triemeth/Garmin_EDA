de4dbf0f886332bba0d40b9ece5ca1125987e6f7
Ethan Triem <EJTriem@outlook.com>
2025-08-13 21:32:07 -0500

    jus starting

---
+++ Connect.py
@@ -0,0 +1,15 @@
+from garminconnect import Garmin
+import datetime
+from datetime import date
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    username = input("Enter email address: ")
+    password = getpass("Enter password: ")
+
+    api = Garmin(username, password).login()
+
+    activity_start_date = datetime.date(2025, 7, 1)
+    activity_end_date = date.today()
